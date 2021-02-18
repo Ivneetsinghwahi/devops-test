998466da9354e7d23d728361534bd2a7813928c8
Ivneetsinghwahi <ivneeet.singh@gmail.com>
2021-02-18 10:56:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var os = require('os')
 var http = require('http')
 
 function handleRequest(req, res) {
-  res.send('hello');
   res.write('Hi there! I\'m being served again from ' + os.hostname());
-  res.write('Doing changes in the file to test the webhook with codepipeline and time taken to redeploy to ec2 and load balancer');
-  res.end();
+  res.end("hello");
 }
 
 http.createServer(handleRequest).listen(3000)
