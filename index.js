c91f5eb164b34cfa1762ca114da7a3b70515d96f
Ivneetsinghwahi <ivneeet.singh@gmail.com>
2021-02-18 11:35:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var os = require('os')
 var http = require('http')
 
 function handleRequest(req, res) {
-  res.write('Hi there! I\'m being served again from ' + os.hostname());
-  res.end("hello");
+  res.write('Hi there! I\'m being served again and again from ' + os.hostname());
+  res.end();
 }
 
 http.createServer(handleRequest).listen(3000)
